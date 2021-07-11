5ddb6b89548f08db48a8f3b2b03f6c9c1594a610
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-11 10:54:40 -0400

    Setup for calender dialog - also setup onclick function that will handle a lot of event.s

--- app/src/main/java/com/example/myfavoritespots/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/AddHappyPlaceActivity.kt
@@ -1,12 +1,19 @@
 package com.example.myfavoritespots
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import java.util.*
 
-class AddHappyPlaceActivity : AppCompatActivity() {
+class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener { //by implemented the onclicklistener extension it requires an onlick fun instead of just doing it in the oncreate
     @SuppressLint("RestrictedApi")
+
+    private val cal = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener //dont have to initialize it now
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
@@ -16,5 +23,18 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         toolbar_add_place.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, month)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
