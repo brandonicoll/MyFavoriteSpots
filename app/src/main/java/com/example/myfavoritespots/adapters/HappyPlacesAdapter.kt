924d411667239c1f9ef99b5f87fc4de70e248904
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-13 18:21:34 -0400

    Setup and created the adapter for the recycler view -> all functions implemented (onCreateViewHolder, onBindViewHolder, getItemCount).

---
+++ app/src/main/java/com/example/myfavoritespots/adapters/HappyPlacesAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.myfavoritespots.adapters
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myfavoritespots.R
+import com.example.myfavoritespots.models.HappyPlaceModel
+import kotlinx.android.synthetic.main.item_happy_place.view.*
+
+open class HappyPlacesAdapter(private val context: Context, private var list: ArrayList<HappyPlaceModel>)
+    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_happy_place, parent, false) //inflates the xml of the cardview
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val model = list[position]
+
+        if (holder is MyViewHolder) {
+            holder.itemView.iv_place_image.setImageURI(Uri.parse(model.image))
+            holder.itemView.tvTitle.text = model.title
+            holder.itemView.tvDescription.text = model.description
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    private class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
+}
