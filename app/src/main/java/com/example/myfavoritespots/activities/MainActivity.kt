8f4cffcb3647645e8741b28876e3fd28ed50f6e3
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-13 17:43:30 -0400

    Implemented function in the main that retrieves the getHappyPlaceList and stores it in the main to be accessed -> no current functionality with it besides testing (to be stored in a recycler view)

--- app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.myfavoritespots.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.myfavoritespots.R
+import com.example.myfavoritespots.database.DatabaseHandler
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,4 +19,16 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun getHappyPlacesListFromLocalDatabase() {
+        val dbHandler = DatabaseHandler(this)
+        val getHappyPlaceList = dbHandler.getHappyPlacesList() //gets happy place list from DatabaseHandler.kt get class
+
+        if (getHappyPlaceList.size > 0) { //for testing until i add a recycler view
+            for (i in getHappyPlaceList) {
+                Log.e("Title", i.title)
+            }
+        }
+    }
+
 }
