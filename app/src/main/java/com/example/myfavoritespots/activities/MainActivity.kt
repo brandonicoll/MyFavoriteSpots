b4e267812b0bb3521f5387af76c7b5ae10506a45
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-13 11:19:37 -0400

    Organized project into different packages -> also created data class to model a happy place.

--- app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.myfavoritespots
+package com.example.myfavoritespots.activities
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myfavoritespots.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
