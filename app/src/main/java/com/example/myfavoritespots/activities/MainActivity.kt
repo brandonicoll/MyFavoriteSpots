bee350f5ff72a09936c29411bdbac5e225addcad
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-14 22:21:30 -0400

    Attached the edit touch helper just made to the happy place list recycler view to add the swipe to edit functionality.

--- app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myfavoritespots.R
@@ -70,6 +71,9 @@ class MainActivity : AppCompatActivity() {
                 adapter.notifyEditItem(this@MainActivity, viewHolder.adapterPosition, ADD_PLACE_ACTIVITY_REQUEST_CODE)
             }
         }
+
+        val editItemTouchHelper = ItemTouchHelper(editSwipeHandler)
+        editItemTouchHelper.attachToRecyclerView(rv_happy_places_list) //attach the touch helper to the recycler view
     }
 
     companion object {
