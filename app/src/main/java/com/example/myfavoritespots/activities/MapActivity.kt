92c2028814d1a4b0fda9d064846579e13eb9510d
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-15 21:23:15 -0400

    Setup toolbar for map activity_map.xml / MapActivity.kt

--- app/src/main/java/com/example/myfavoritespots/activities/MapActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/activities/MapActivity.kt
@@ -3,10 +3,19 @@ package com.example.myfavoritespots.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myfavoritespots.R
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
+import kotlinx.android.synthetic.main.activity_add_happy_place.toolbar_add_place
+import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        setSupportActionBar(toolbar_map)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toolbar_map.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
