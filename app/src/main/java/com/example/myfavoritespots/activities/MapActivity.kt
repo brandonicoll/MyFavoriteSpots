34a684761b23e68398e58a4dce43cd6cf0be4ee1
Brandon <brandonspersonalmailbox@gmail.com>
2021-07-15 22:07:46 -0400

    Checking if the intent has extra info and setting up the happyplacemodel and toolbar based if it does.

--- app/src/main/java/com/example/myfavoritespots/activities/MapActivity.kt
+++ app/src/main/java/com/example/myfavoritespots/activities/MapActivity.kt
@@ -3,19 +3,31 @@ package com.example.myfavoritespots.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myfavoritespots.R
+import com.example.myfavoritespots.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_add_happy_place.*
 import kotlinx.android.synthetic.main.activity_add_happy_place.toolbar_add_place
 import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity() {
+
+    private var mHappyPlaceDetails: HappyPlaceModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
-        setSupportActionBar(toolbar_map)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toolbar_map.setNavigationOnClickListener {
-            onBackPressed()
+        if (intent.hasExtra(MainActivity.EXTRA_PLACE_DETAILS)) {
+            mHappyPlaceDetails = intent.getParcelableExtra(MainActivity.EXTRA_PLACE_DETAILS) as HappyPlaceModel?
         }
+
+        if (mHappyPlaceDetails != null) {
+            setSupportActionBar(toolbar_map)
+            supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+            supportActionBar!!.title = mHappyPlaceDetails!!.title
+            toolbar_map.setNavigationOnClickListener {
+                onBackPressed()
+            }
+        }
+
     }
 }
